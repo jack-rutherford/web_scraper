0832077aac56c3eeabc5c48ff3817e5a474fa298
Jack Rutherford <jack.rutherford@hope.edu>
2024-01-02 22:06:28 -0500

    Web scraper writes to file now that keeps track of posts. Also edited gitignore so that it doesn't track each post that writes to the folder.

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
+import time
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=').text
-soup = BeautifulSoup(html_text, 'lxml')
-jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
-for job in jobs:
-    published_date = job.find('span', class_ = 'sim-posted').span.text.strip()
-    if 'few' in published_date:
-        company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
-        skills = job.find('span', class_ = 'srp-skills').text.replace(' ', '').replace(',', ', ')
-        more_info = job.header.h2.a['href']
-        print(f'Company Name: {company_name.strip()}')
-        print(f'Required Skills: {skills.strip()}')
-        print(f'More Info: {more_info}')
-        print('')
 
-# with open('web_scraper/home.html', 'r') as html_file:
-#     content = html_file.read()
-    
-#     soup = BeautifulSoup(content, 'lxml')
-#     course_cards = soup.find_all('div', class_='card')
-#     for course in course_cards:
-#         course_name = course.h5.text
-#         course_price = course.a.text.split()[-1]
 
-#         print(f'{course_name} costs {course_price}')
+# Add it so you can filter multiple skills
+print('Put some skill that you are not familiar with.')
+print('Separate the skills with a comma (python, django, linux)')
+unfamiliar_skill = input('>')
+print(f'Filtering out {unfamiliar_skill}')
+# unfamiliar_skill = unfamiliar_skill.split(',')
+# print(f'Filtering out {*unfamiliar_skill,}')
+
+
+def find_jobs():
+    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+    for index, job in enumerate(jobs):
+        published_date = job.find('span', class_ = 'sim-posted').span.text.strip()
+        if 'few' in published_date:
+            company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
+            skills = job.find('span', class_ = 'srp-skills').text.replace(' ', '').replace(',', ', ')
+            more_info = job.header.h2.a['href']
+            if unfamiliar_skill not in skills:
+                with open(f'posts/{index}.txt', 'w') as f:
+                    f.write(f'Company Name: {company_name.strip()}')
+                    f.write(f'Required Skills: {skills.strip()}')
+                    f.write(f'More Info: {more_info}')
+                print(f'File saved: {index}')
+
+if __name__ == '__main__':
+    while True:
+        find_jobs()
+        time_wait = 10
+        print(f'Waiting {time_wait} minutes...')
+        time.sleep(time_wait * 60) # 10 mins
+
